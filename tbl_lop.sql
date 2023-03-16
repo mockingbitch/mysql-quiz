83a80a28ba9a9b604ca3c82c199c3b6d674ed604
mockingbitch <jarvis.phongtran@gmail.com>
2023-03-16 16:54:32 +0700

    tbl lop sql

---
+++ tbl_lop.sql
@@ -0,0 +1,7 @@
+INSERT INTO `tbl_lop` (`id`, `ten_lop`) VALUES
+(1,	'Lớp Java'),
+(2,	'Lớp PHP'),
+(3,	'Lớp JavaScript'),
+(4,	'Lớp Java Spring nâng cao'),
+(5,	'Lớp ReactJs nâng cao'),
+(6,	'Lớp Laravel nâng cao');
